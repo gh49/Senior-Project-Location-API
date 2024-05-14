ecb83c218357ccd826047efb45bf45af658916de
ghassan <ghassan1000@live.com>
2024-05-14 03:31:11 +0300

    v2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 let locations = [];
 
-let stations = [2, 75, 170, 320, 510, 658, 863];
+let stations = [0, 75, 170, 320, 510, 658, 865];
 
 fs.readFile("tram_path.json", "utf8", (err, data) => {
     if (err) {
@@ -54,14 +54,14 @@ function areEqualDouble(num1, num2, epsilon = Number.EPSILON) {
 }
 
 const io = socket(server)
-let speed = 200;
+let speed = 100;
 let interval = 7200/speed;
 
 let currentIndex = 0;
 let indexFactor = 1;
 
 function emitNextLocation() {
-    console.log(currentIndex);
+    //console.log(currentIndex);
     const location = locations[currentIndex];
     io.emit('location', location);
 
@@ -91,20 +91,109 @@ function shouldPauseStreaming() {
 
 startStreaming(interval);
 
+const pauseDuration = 3;
+
 setInterval(() => {
     if (shouldPauseStreaming() && !isPaused) {
         stopStreaming();
         isPaused = true;
-        const pauseDuration = 3000;
-        console.log("Streaming paused for " + pauseDuration + " milliseconds.");
+        console.log("Streaming paused for " + pauseDuration + " seconds.");
         setTimeout(() => {
+            if(currentIndex == 0) {
+                indexFactor = 1;
+            }
+            else if(currentIndex == locations.length - 1) {
+                indexFactor = -1;
+            }
             currentIndex = currentIndex + 1*indexFactor;
             startStreaming(interval);
             isPaused = false;
             console.log("Streaming resumed.");
-        }, pauseDuration);
+        }, pauseDuration*1000);
     }
 }, 10);
+
+const srcStation = stations[2];
+const dstStation = stations[6];
+
+function tramDirectlyToStation() {
+    if(currentIndex == srcStation) {
+        return true;
+    }
+    if(currentIndex > srcStation && indexFactor == -1) {
+        return true;
+    }
+    if(currentIndex < srcStation && indexFactor == 1) {
+        return true;
+    }
+    return false;
+}
+
+setInterval(emitEstimation, 3000);
+
+function emitEstimation() {
+    if(isPaused) {
+        return;
+    }
+    let time;
+    if(currentIndex == srcStation) {
+        time = 0;
+    }
+    else if(tramDirectlyToStation()) {
+        time = simpleEstimator(currentIndex, srcStation, 1);
+    }
+    else {
+        time = complexEstimator();
+    }
+
+    console.log(`time left: ${time}`);
+    io.emit('time', time);
+}
+
+function simpleEstimator(currentIndex, srcStation, minusFactor) {
+    const distanceToSrc = Math.abs((currentIndex - srcStation) * 2);
+    let stationsNum = Math.abs(stationsBetween(currentIndex, srcStation) - minusFactor);
+    const totalTime = 3.6 * (distanceToSrc / speed) + stationsNum * pauseDuration;
+    return totalTime;
+}
+
+function complexEstimator() {
+    let end;
+    if(currentIndex > srcStation) {
+        end = locations.length - 1;
+    }
+    else {
+        end = 0;
+    }
+    const totalTime = simpleEstimator(currentIndex, end, 0) + simpleEstimator(end, srcStation, 0) - pauseDuration;
+    return totalTime;
+}
+
+function stationsBetween(p1, p2) {
+    let count = 0;
+    let big, small;
+    if(p1 > p2) {
+        big = p1;
+        small = p2;
+    }
+    else {
+        big = p2;
+        small = p1;
+    }
+    for(let i = 0; i < stations.length; i++) {
+        if(inRange(stations[i], small, big)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function inRange(point, min, max) {
+    if(point >= min && point <= max) {
+        return true;
+    }
+    return false;
+}
 
 io.on('connection', (socket) => {
     console.log('A client connected');
